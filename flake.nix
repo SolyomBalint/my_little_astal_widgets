9185722d0cab13e408d2362c2e8767fa5ad3e72f
SolyomBalint <balint.solyom01@gmail.com>
2025-01-19 13:36:53 +0100

    Add Initial Working Files
    Added the initial files where ags widgets work with astal config. Also a
    flake.nix is added so that the project can be downloaded with flakes.
    Everything is from available templates atm.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, astal }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = astal.lib.mkLuaPackage {
+      inherit pkgs;
+      name = "Start-astal-widgets";
+      src = ./.;
+
+      extraPackages = [
+      ];
+    };
+  };
+}
